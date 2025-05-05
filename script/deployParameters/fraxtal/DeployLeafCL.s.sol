facd9f7d1fe0d98ce2af1268b27481d367e33e63
Ricardo Airton <58168802+airtoonricardo@users.noreply.github.com>
2025-05-05 16:03:00 +0100

    chore: redeploy fraxtal (#91)
    * chore: only deploy swaprouter in leaf deployscript
    
    * chore: update weth addr in fraxtal deployment

--- script/deployParameters/fraxtal/DeployLeafCL.s.sol
+++ script/deployParameters/fraxtal/DeployLeafCL.s.sol
@@ -7,7 +7,7 @@ import "script/01_DeployLeafBaseFixture.s.sol";
 contract DeployLeafCL is DeployLeafBaseFixture {
     function setUp() public override {
         _params = DeployLeafBaseFixture.DeploymentParameters({
-            weth: 0xFC00000000000000000000000000000000000006,
+            weth: 0xFc00000000000000000000000000000000000002,
             leafVoter: 0x97cDBCe21B6fd0585d29E539B1B99dAd328a1123,
             factoryV2: 0x31832f2a97Fd20664D76Cc421207669b55CE4BC0,
             xVelo: 0x7f9AdFbd38b669F03d1d11000Bc76b9AaEA28A81,
